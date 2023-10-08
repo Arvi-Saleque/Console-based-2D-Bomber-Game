53847a47a5da1db5004bb9975251aaacdc3661e9
Arvi_Saleque <alifsalek.as@gmail.com>
2023-10-08 22:51:45 +0600

    Delete Bomber Game directory

--- Bomber Game/bomb.h
+++
@@ -1,8 +0,0 @@
-class Bomb {
-    int time, id;
-    COORD position;
-    bool flag;
-public:
-    Bomb(int i, int _t, COORD _p, bool _f) : id(i), time(_t), position(_p), flag(_f) {}
-    friend class Game;
-};
